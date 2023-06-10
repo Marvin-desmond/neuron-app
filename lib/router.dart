d0128961aeffe832fa5ea722a14861d739002c0e
Marvin-Mboya <marvinmboya@Marvins-PC.local>
2023-06-10 17:31:50 +0300

    router change to account for file_progress and test_screen removal

--- lib/router.dart
+++ lib/router.dart
@@ -1,16 +1,11 @@
-import 'package:flutter_native/file_progress.dart';
 import 'package:flutter_native/global.dart';
 import 'package:flutter_native/catalog/model_catalog.dart';
-import 'package:flutter_native/test_screen.dart';
 import 'package:flutter_native/model_cards.dart';
 import 'package:flutter_native/playground/model_playground.dart';
-import 'package:flutter_native/ml_store/ml_model_class.dart';
 
 class ScreenPaths {
   static String home = 'home';
-  static String modelCards = 'model_cards';
   static String modelPlayground = 'model_playground';
-  static String fileProgress = "file_progress";
   static String modelCatalog = "model_catalog";
 }
 
@@ -20,16 +15,9 @@ final GoRouter appRouter = GoRouter(
       name: ScreenPaths.home,
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
-        return const TestApp();
+        return ModelCards();
       },
       routes: <RouteBase>[
-        GoRoute(
-          name: ScreenPaths.modelCards,
-          path: "model_cards",
-          builder: (BuildContext context, GoRouterState state) {
-            return ModelCards();
-          },
-        ),
         GoRoute(
           name: ScreenPaths.modelPlayground,
           path: "model_playground",
@@ -39,13 +27,6 @@ final GoRouter appRouter = GoRouter(
               tag: state.queryParameters['tag']!,
               playground: state.queryParameters['playground']!,
             );
-          },
-        ),
-        GoRoute(
-          name: ScreenPaths.fileProgress,
-          path: "file_progress",
-          builder: (BuildContext context, GoRouterState state) {
-            return const FileProgress();
           },
         ),
         GoRoute(
